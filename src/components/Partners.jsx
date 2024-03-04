8740cfbb7a875418a680f9b0846c825877da33d8
FM-Akib <muntasirakib08@gmail.com>
2024-03-04 17:25:01 +0600

    updated

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -5,11 +5,13 @@ import h3 from '../assets/hospitsLogo/h3.jpg'
 import h4 from '../assets/hospitsLogo/h4.jpg'
 import h5 from '../assets/hospitsLogo/h5.png'
 import h6 from '../assets/hospitsLogo/h7.jpg'
+import h7 from '../assets/hospitsLogo/h6.jpg'
+import h8 from '../assets/hospitsLogo/h8.png'
 
 const Partners = () => {
     return (
         <>
-        <h1 className="text-center text-2xl font-bold leadi sm:text-2xl p-2">Partners Hospital</h1>
+        <h1 className="text-orange-500 text-center text-2xl font-bold leadi sm:text-2xl p-2">Partners Hospital</h1>
        <Marquee className="h-24 bg-white">
                <div className="h-20 p-2 mr-12">
                 <img className="h-full" src={h1} alt=""/>
@@ -28,6 +30,12 @@ const Partners = () => {
                </div>
                <div className="h-20 p-2 mr-12">
                 <img className="h-full" src={h6} alt=""/>
+               </div>
+               <div className="h-20 p-2 mr-12">
+                <img className="h-full" src={h7} alt=""/>
+               </div>
+               <div className="h-20 p-2 mr-12">
+                <img className="h-full" src={h8} alt=""/>
                </div>
        </Marquee>
        </>
