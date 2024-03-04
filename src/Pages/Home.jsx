e23a886010831ce7996d466a69dfeab7679ff503
FM-Akib <muntasirakib08@gmail.com>
2024-03-04 22:06:38 +0600

    updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,23 @@
+import { useLoaderData } from "react-router-dom";
 import Hero from "../components/Hero";
 import Partners from "../components/Partners";
+import DoctorCard from "../components/DoctorCard";
 
 const Home = () => {
+    const doctors = useLoaderData();
+    console.log(doctors)
     return (
         <div>
             <Hero></Hero>
             <Partners></Partners>
+
+            <div className="bg-gray-100 px-16 py-8 grid grid-cols-2 justify-center items-center gap-4 mx-auto border-8">
+                {
+                    doctors.map(adoctor=> <DoctorCard key={adoctor._id}
+                    doctor={adoctor}
+                    ></DoctorCard>)
+                }
+            </div>
         </div>
     );
 };
