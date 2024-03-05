4e55db8aecbb367ee9eddfd00469af73ed798baf
FM-Akib <muntasirakib08@gmail.com>
2024-03-05 16:43:33 +0600

    authentication

---
+++ src/Authentication/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKY0NYjo9PNMqfwyEZH7mEs5AfusJEQDU",
+  authDomain: "doctors-ticket.firebaseapp.com",
+  projectId: "doctors-ticket",
+  storageBucket: "doctors-ticket.appspot.com",
+  messagingSenderId: "786540157571",
+  appId: "1:786540157571:web:e0272832872a5256d259b6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
