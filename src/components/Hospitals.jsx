550da9077d9aa10d77f1d1635769cce627593985
FM-Akib <muntasirakib08@gmail.com>
2024-03-10 01:04:28 +0600

    updated

--- src/components/Hospitals.jsx
+++ src/components/Hospitals.jsx
@@ -94,27 +94,6 @@ const Hospitals = () => {
 
 
 
-  {/* <div id="slide2" className="carousel-item relative w-full">
-    <img src={c2} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide3" className="carousel-item relative w-full">
-    <img src={c3} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" className="btn btn-circle">❮</a> 
-      <a href="#slide4" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide4" className="carousel-item relative w-full">
-    <img src={c4} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
-    </div>
-  </div> */}
     </div>
 
     </div>
